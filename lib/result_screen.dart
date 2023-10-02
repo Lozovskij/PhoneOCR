0ac24012b2cf74cec37e10127abd5ed36246bae4
Ivan Lozovskij <ivan.lozovskij1@gmai.com>
2023-10-02 17:13:03 +0300

    refactor: remove comments

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -53,17 +53,12 @@ class PhoneItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final theme = Theme.of(context);
-    // final style = theme.textTheme.displaySmall!.copyWith(
-    //   color: theme.colorScheme.onPrimary,
-    // );
     final theme = Theme.of(context);
     final style = theme.textTheme.displaySmall!.copyWith(
       color: theme.colorScheme.primary,
     );
 
     return Card(
-      // color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Column(
